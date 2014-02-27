2c3a3a416b4e5047d767acf472b0a1f42e8b7a9b
André <andre_g_faria@hotmail.com>
2014-02-27 14:23:52 -0300

    Melhorias no singleton (agora a relação de letras é criada junto com a instância da classe LetraFactory).

--- Navigation/Navigation/LetraFactory.m
+++ Navigation/Navigation/LetraFactory.m
@@ -10,28 +10,28 @@
 
 @implementation LetraFactory
 
-
+static NSDictionary *relacaoLetras;
 
 +(id)allocWithZone:(struct _NSZone *)zone {
-    return [LetraFactory sharedInstance];
+        return [LetraFactory sharedInstance];
 }
 
 +(LetraFactory *) sharedInstance {
     static LetraFactory *instancia = nil;
+    
     if (!instancia) {
         instancia = [[super allocWithZone:nil] init];
-        
+        if (!relacaoLetras) {
+            relacaoLetras = [[NSDictionary alloc] initWithObjects:
+                             @[@"Avião",@"Barco",@"Casa",@"Dado",@"Escola",@"Faca",@"Gato",@"Hamster",@"Igreja",@"Joaninha",@"Kiwi",@"Lápis",@"Mesa",@"Navio",@"Olho",@"Pato",@"Queijo",@"Rato",@"Sapo",@"Tesoura",@"Uva",@"Vento",@"Waffle",@"Xícara",@"Youtube",@"Zebra" ]
+                                                          forKeys:@[@"A",@"B",@"C",@"D",@"E",@"F",@"G",@"H",@"I",@"J",@"K",@"L",@"M",@"N",@"O",@"P",@"Q",@"R",@"S",@"T",@"U",@"V",@"W",@"X",@"Y",@"Z" ]];
+        }
+
     }
     return instancia;
 }
 
 -(Letra *)getLetra:(char)c {
-    static NSDictionary *relacaoLetras = nil;
-    if (!relacaoLetras) {
-        relacaoLetras = [[NSDictionary alloc] initWithObjects:
-                         @[@"Avião",@"Barco",@"Casa",@"Dado",@"Escola",@"Faca",@"Gato",@"Hamster",@"Igreja",@"Joaninha",@"Kiwi",@"Lápis",@"Mesa",@"Navio",@"Olho",@"Pato",@"Queijo",@"Rato",@"Sapo",@"Tesoura",@"Uva",@"Vento",@"Waffle",@"Xícara",@"Youtube",@"Zebra" ]
-                                                      forKeys:@[@"A",@"B",@"C",@"D",@"E",@"F",@"G",@"H",@"I",@"J",@"K",@"L",@"M",@"N",@"O",@"P",@"Q",@"R",@"S",@"T",@"U",@"V",@"W",@"X",@"Y",@"Z" ]];
-    }
     UIImage *imagem = [UIImage imageNamed:[NSString stringWithFormat:@"img%c.png",c]];
     Letra *l = [[Letra alloc] initWithLetra:c ePalavra:[relacaoLetras objectForKey:[NSString stringWithFormat:@"%c",c]] eImagem:imagem];
     return l;
